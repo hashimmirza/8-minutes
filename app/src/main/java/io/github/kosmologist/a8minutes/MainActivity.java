919d5a7e48d84dbd6aa8891d405cbb072f957880
Muhammad Hashim <hashu.mughal@gmail.com>
2018-04-21 00:07:57 +0500

    UI

---
+++ app/src/main/java/io/github/kosmologist/a8minutes/MainActivity.java
@@ -0,0 +1,18 @@
+package io.github.kosmologist.a8minutes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
